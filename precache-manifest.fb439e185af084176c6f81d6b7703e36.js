b3dc80a97dee01bffb809936ddf521d1a642ab41
dl541 <dl541@cam.ac.uk>
2019-06-01 23:15:30 +0100

    Updates

---
+++ precache-manifest.fb439e185af084176c6f81d6b7703e36.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ac242b8ef0531d990e7261bfd48325",
+    "url": "/todo-list-react-app/index.html"
+  },
+  {
+    "revision": "a66f5494715de47b0f67",
+    "url": "/todo-list-react-app/static/js/2.6376b262.chunk.js"
+  },
+  {
+    "revision": "f9c635c12d8e82108dfc",
+    "url": "/todo-list-react-app/static/js/main.3e32a93c.chunk.js"
+  },
+  {
+    "revision": "325af6991ef8c86faff4",
+    "url": "/todo-list-react-app/static/js/runtime~main.bc9aef26.js"
+  }
+]);
